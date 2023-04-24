bae59d71a8367986c7d4fc5f4be02075cf70b885
Anailton Silva <francisco.anailton17@gmail.com>
2023-04-24 20:00:38 -0300

    aplicando

--- main2.py
+++ main2.py
@@ -3,15 +3,24 @@ from system import Cadastro
 p1 = Pessoa()
 c1 = Cadastro(p1)
 lista = list()
-resp = 's'
-while resp != 'n':
-    nome = input('Nome: ')
-    idade = input('Idade: ')
+resp = 1
+while resp != 2:
+    nome = str(input('Nome: '))
+    idade = str(input('Idade: '))
+    if idade.isnumeric():
+        idade = int(idade)
+    else:
+        print('ERRO...DIGITE APENAS NUMERO')
     p1 = Pessoa(nome, idade)
     c1 = Cadastro(p1)
     c1.armazenar()
     lista.append(c1.salvar.copy())
-    resp = str(input('Deseja continuar? '))
+    print('[ 1 ] PARA CONTINUAR.\n[ 2 ] PARA PARAR.')
+    try:
+        resp = int(input('Escolha uma Opção:'))
+    except:
+        print('ERRO!!')
+        print('OPÇÃO INVÀLIDA, DIGITE NOVAMENTE.')
 
 for i in lista:
     for k, x in enumerate(i):
